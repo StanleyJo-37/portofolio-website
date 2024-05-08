429a445ff35a0d619f5ba863b68d2c0ebd747722
StanleyJo-37 <stanleyjo13w@gmail.com>
2024-05-08 23:08:11 +0700

    Changed CV filename and Temporarily Removed Favicon

--- frontend/src/Pages/CV.tsx
+++ frontend/src/Pages/CV.tsx
@@ -8,8 +8,8 @@ const CV: React.FC = () =>
     return (
         <>
             <iframe 
-                title='CV'
-                src="/pdf/CV Stanley Jonathan Wahjudi.pdf"
+                title='CV Stanley Jonathan Wahjudi-Mei-2024'
+                src="/pdf/CV Stanley Jonathan Wahjudi-Mei-2024.pdf"
                 style={{
                     width: "100vw",
                     height: "100vh",
